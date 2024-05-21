5a6edd591abd658524e99f88fdc9b66eb903a00f
AlbertoDelgado2 <albertitomdz013b@gmail.com>
2024-05-21 14:04:44 +0200

    PARAENTREGAR

---
+++ components/ProductDetail.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="grid grid-cols-2">
+        <div>
+<img :src="product.image" alt="">
+    </div>
+    <div>
+        <h2>{{ product.title }}</h2>
+        <p> {{ ProductCard.price }}</p>
+        <h3>{{ product.desciption }}</h3>
+    </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
